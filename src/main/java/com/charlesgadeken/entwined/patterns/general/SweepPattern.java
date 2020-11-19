984abc0c3863e96bafe2959f116bbc7a26ffffaa
meawoppl <meawoppl@gmail.com>
2020-11-18 19:57:46 -0800

    Dem file dem files, dem new files

---
+++ src/main/java/com/charlesgadeken/entwined/patterns/general/SweepPattern.java
@@ -0,0 +1,2 @@
+package com.charlesgadeken.entwined.patterns.general;public class SweepPattern {
+}
---
+++ src/test/java/com/charlesgadeken/entwined/patterns/general/SweepPatternTest.java
@@ -0,0 +1,6 @@
+package com.charlesgadeken.entwined.patterns.general;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SweepPatternTest {
+  
+}
